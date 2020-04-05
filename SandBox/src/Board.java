3ee10788c69b0c18b4a4f4d8a3c5466720b6c60d
nico <nicobod31@gmail.com>
2020-04-05 17:51:05 +0200

    re-factorization of code for clarity

---
+++ SandBox/src/Board.java
@@ -0,0 +1,81 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+class Board {
+    private static int EMPTY = 0;
+    private static int ISLAND = 1;
+
+    private int nbrCellX;
+    private int nbrCellY;
+    private int map[][];
+    private List<Cell> listCellAlreadyVisited;
+    private List<Sector> listSecteurs;
+
+    // constructor
+    public Board() {
+    }
+
+    public Board(int nbrCellX, int nbrCellY, int[][] map, List<Cell> listCellAlreadyVisited, List<Sector> listSecteurs) {
+        this.nbrCellX = nbrCellX;
+        this.nbrCellY = nbrCellY;
+        this.map = new int[nbrCellX][nbrCellY];
+        this.listCellAlreadyVisited = listCellAlreadyVisited;
+        this.listSecteurs = listSecteurs;
+    }
+
+    // getters setters
+    public int getNbrCellX() {
+        return nbrCellX;
+    }
+    public void setNbrCellX(int nbrCellX) {
+        this.nbrCellX = nbrCellX;
+    }
+    public int getNbrCellY() {
+        return nbrCellY;
+    }
+    public void setNbrCellY(int nbrCellY) {
+        this.nbrCellY = nbrCellY;
+    }
+    public List<Cell> getListCellAlreadyVisited() {
+        return listCellAlreadyVisited;
+    }
+    public void setListCellAlreadyVisited(List<Cell> listCellAlreadyVisited) {
+        this.listCellAlreadyVisited = listCellAlreadyVisited;
+    }
+    public List<Sector> getListSecteurs() {
+        return listSecteurs;
+    }
+    public void setListSecteurs(List<Sector> listSecteurs) {
+        this.listSecteurs = listSecteurs;
+    }
+    public int[][] getMap() {
+        return map;
+    }
+
+
+    // to string
+    @Override
+    public String toString() {
+        return "Board{" +
+                "nbrCellX=" + nbrCellX +
+                ", nbrCellY=" + nbrCellY +
+                ", listCellAlreadyVisited=" + listCellAlreadyVisited +
+                ", listSecteurs=" + listSecteurs +
+                '}';
+    }
+
+    public void createBiDirectionalTable(List<String> stringMap) {
+        for (int i = 0; i < stringMap.size(); i++) {
+            for (int j = 0; j < stringMap.get(i).length(); j++) {
+                map[i][j] = stringMap.get(j).charAt(i) == 'x' ? ISLAND : EMPTY;
+                if (map[i][j] == ISLAND) {
+                    System.err.println("ISLAND = " + i + j);
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ SandBox/src/Deploy.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * to deploy in real water!
+ */
+class Deploy {
+    private static int EMPTY = 0;
+    private static int ISLAND = 1;
+
+    public String deploy0(Board board) {
+        int map[][] = board.getMap();
+
+        if (map[0][14] == EMPTY) {
+            return "0 14";
+        } else {
+            return "7 14";
+        }
+    }
+}
